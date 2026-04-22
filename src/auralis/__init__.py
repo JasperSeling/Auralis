7ea5187cf31411a5f9c6677de062fdf67adaedac
JasperSeling <niksuasji@gmail.com>
2026-04-22 13:44:41 +0300

    Revert "Remove project files"
    This reverts commit 1457b8a64d921856a12c6b29a9254e1c1cc9b981.

---
+++ src/auralis/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+os.environ.setdefault("VLLM_ENABLE_V1_MULTIPROCESSING", "0")
+
+from auralis.core.tts import TTS
+from auralis.common.definitions.requests import TTSRequest
+from auralis.common.definitions.output import TTSOutput
+from auralis.common.definitions.enhancer import AudioPreprocessingConfig
+from auralis.common.logging.logger import setup_logger, set_vllm_logging_level
+
+__all__ = [
+    "TTS",
+    "TTSRequest",
+    "TTSOutput",
+    "AudioPreprocessingConfig",
+    "setup_logger",
+    "set_vllm_logging_level",
+]
